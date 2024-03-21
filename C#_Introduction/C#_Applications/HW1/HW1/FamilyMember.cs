313778f316ffaea1e7f4f46e97f14dbb55fc678d
ksana-sidoruk <ksana.sidoruk@gmail.com>
2024-03-21 18:13:10 +0100

    C#_Applications HW1

---
+++ C#_Introduction/C#_Applications/HW1/HW1/FamilyMember.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+
+        /*
+        // то же самое, что делается { get; set; }
+        private string _name;
+        
+        public string GetValueName()
+        {
+            return _name;
+        }
+
+        public void SetValueName(string value)
+        {
+            _name = value;
+        }
+        */
+
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public uint Age { get; set; }
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public FamilyMember[] Siblings { get; set; }
+        public List<FamilyMember> WholeFamily { get; set; }
+        public FamilyMember Spouse { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name} (age: {Age}, gender {Gender})";
+        }
+
+        //выводит информацию о родителях
+        public void ShowParents()
+        {
+            if (Father != null)
+            {
+                Console.WriteLine($"Father: {Father.Name} ");
+            }
+            else
+            {
+                Console.WriteLine("No info on the father");
+            }
+
+            if (Mother != null)
+            {
+                Console.WriteLine($"Mother: {Mother.Name} ");
+            }
+            else
+            {
+                Console.WriteLine("No info on the mother");
+            }
+        }
+
+        //два следующих метода для вывода информации о предках рекурсивно, вариант не учитывает сиблингов
+        public void PrintAllFamily()
+        {
+            PrintFamilyMember(this);
+        }
+        public void PrintFamilyMember(FamilyMember familyMember)
+        {
+            Console.WriteLine(familyMember);
+            if (familyMember.Father != null)
+            {
+                PrintFamilyMember(familyMember.Father);
+            }
+            if (familyMember.Mother != null)
+            {
+                PrintFamilyMember(familyMember.Mother);
+            }
+        }
+
+        //сортирует всех родственников по убыванию возраста (для того, чтоб вывести информацию обо всех родственниках this
+        public void SortFamilyByAge()
+        {
+            WholeFamily = WholeFamily.OrderByDescending(member => member.Age).ToList();
+        }
+
+        //метод, чтобы проверить, состоит ли объект в браке
+        private bool IsMarried()
+        {
+            if (this.Spouse != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //_________________________________________________________________________
+        // ДОМАШНЕЕ ЗАДАНИЕ:
+        public void PrintFamilies()
+        {
+            SortFamilyByAge();
+            Console.WriteLine($"Here are family members of {this.Name} sorted by descending by age");
+            foreach (var member in this.WholeFamily)
+            {
+                Console.WriteLine($"Name: {member.Name}, age: {member.Age}");
+            }
+            if (this.IsMarried())
+            {
+                Spouse.SortFamilyByAge();
+                Console.WriteLine($"\nAnd here are family members of {Spouse.Name}, {this.Name}' spouse, sorted by descending by age");
+                foreach (var member in Spouse.WholeFamily)
+                {
+                    Console.WriteLine($"Name: {member.Name}, age: {member.Age}");
+                }
+            }
+        }
+    }
+}
---
+++ C#_Introduction/C#_Applications/HW1/HW1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FamilyMember mother = new() { Name = "Helen", Age = 64, Gender = Gender.Female };
+            FamilyMember father = new() { Name = "Wes", Age = 62, Gender = Gender.Male };
+            FamilyMember zeroMember = new()
+            {
+                Name = "Ksana",
+                Age = 36,
+                Gender = Gender.Female,
+                Father = father,
+                Mother = mother
+            };
+
+            FamilyMember granny = new() { Name = "Pavel", Age = 95, Gender = Gender.Male };
+            zeroMember.Mother.Father = granny;
+
+            FamilyMember meemaw = new() { Name = "Zina", Age = 85, Gender = Gender.Female, Spouse = granny };
+            zeroMember.Mother.Mother = meemaw;
+            meemaw.Spouse = granny;
+
+            FamilyMember babushkaValya = new() { Name = "Valua", Age = 87, Gender = Gender.Female };
+            zeroMember.Father.Mother = babushkaValya;
+
+            FamilyMember aunt = new() { Name = "Oxana", Age = 70, Gender = Gender.Female, Father = granny, Mother = meemaw };
+            FamilyMember brother = new() { Name = "Roma", Age = 25, Gender = Gender.Female, Father = new FamilyMember { Name = "Gena" } };
+
+            zeroMember.Siblings = [brother];
+            mother.Siblings = [aunt];
+
+            zeroMember.WholeFamily = new List<FamilyMember> { zeroMember, mother, father, meemaw, granny, babushkaValya, brother, aunt };
+            brother.WholeFamily = new List<FamilyMember> { zeroMember, mother, father, meemaw, granny, babushkaValya, brother, aunt };
+
+            /*
+            zeroMember.SortFamilyByAge();
+
+            Console.WriteLine($"The oldest family member of {zeroMember.Name} is {zeroMember.WholeFamily[0]} \n" +
+                $"the whole family members ordered by age can be found below");
+            foreach (var member in zeroMember.WholeFamily)
+            {
+                Console.WriteLine($"Name: {member.Name}, age: {member.Age}");
+            }
+            */
+            //zeroMember.PrintAllFamily();
+            //mother.ShowParents();
+
+            FamilyMember olga = new() { Name = "Olga", Age = 20, Gender = Gender.Female, Spouse = brother };
+            brother.Spouse = olga;
+            FamilyMember nik = new() { Name = "Nikita", Age = 15, Gender = Gender.Male };
+            FamilyMember marina = new() { Name = "Marina", Age = 40, Gender = Gender.Female };
+            olga.Siblings = [nik];
+            olga.WholeFamily = new List<FamilyMember> { marina, olga, nik };
+
+            //_____________________________________________________________________________
+            //Код для проверки выполнения ДЗ:
+
+            brother.PrintFamilies();
+        }
+    }
+}
