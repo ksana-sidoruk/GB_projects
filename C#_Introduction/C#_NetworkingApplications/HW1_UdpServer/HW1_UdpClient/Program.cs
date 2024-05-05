7b02000258395db2c5230736062d5f1f7b7b84fa
ksana-sidoruk <ksana.sidoruk@gmail.com>
2024-05-05 18:07:57 +0200

    HW1_first chat

---
+++ C#_Introduction/C#_NetworkingApplications/HW1_UdpServer/HW1_UdpClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using HW1_UdpServer;
+
+namespace HW1_UdpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessage("This Computer", "127.0.0.1");
+        }
+
+        static void SendMessage(string from, string ipAddress)
+        {
+
+            using (var udpClient = new UdpClient())
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), 12345);
+
+                while (true)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("Please insert the message text");
+                    string messageText = Console.ReadLine();
+
+                    Message message = new Message() { Text = messageText, NameFrom = from, NameTo = "Server", DateTime = DateTime.Now };
+
+                    string json = message.SerializeMessageToJson();
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+
+                    udpClient.Send(data, data.Length, ep);
+
+                    var serverEp = new IPEndPoint(IPAddress.Any, 0);
+                    var replyBuffer = udpClient.Receive(ref serverEp);
+                    var answer = Encoding.UTF8.GetString(replyBuffer);
+
+                    Console.WriteLine(answer);
+
+                    Console.WriteLine("print smth to proceed");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+
+    }
+}
---
+++ C#_Introduction/C#_NetworkingApplications/HW1_UdpServer/HW1_UdpServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HW1_UdpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpServer();
+        }
+
+        public static void UdpServer()
+        {
+            using (var udpClient = new UdpClient(12345))
+            {
+                Console.WriteLine("Sever awaits for a message from the Client...");
+
+
+                while (true)
+                {
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+                    var buffer = udpClient.Receive(ref ep);
+
+                    var messageText = Encoding.UTF8.GetString(buffer);
+                    Message message = Message.DeserializeMessageFromJson(messageText);
+                    message.Print();
+
+                    byte[] reply = Encoding.UTF8.GetBytes("\t\t\tMessage received");
+                    udpClient.Send(reply, reply.Length, ep);
+                }
+            }
+        }
+    }
+}
+
+//ДЗ: только сервер
