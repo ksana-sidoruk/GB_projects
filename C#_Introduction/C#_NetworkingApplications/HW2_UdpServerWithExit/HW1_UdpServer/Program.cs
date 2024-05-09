8be54a43e135925d50718664672514f47f92f7fe
ksana-sidoruk <ksana.sidoruk@gmail.com>
2024-05-09 12:51:20 +0200

    HW2 Udp Server with Exit

---
+++ C#_Introduction/C#_NetworkingApplications/HW2_UdpServerWithExit/HW1_UdpServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HW2_UdpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpServer();
+        }
+
+        public static void ProcessMessage(UdpClient udpClient)
+        {
+            while (true)
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+                var buffer = udpClient.Receive(ref ep);
+
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Message message = Message.DeserializeMessageFromJson(messageText);
+                    message.Print();
+
+                    byte[] reply = Encoding.UTF8.GetBytes("\t\t\tMessage received at " + DateTime.Now);
+                    udpClient.Send(reply, ep);
+
+                });
+            }
+        }
+        public static void UdpServer()
+        {
+            using (var udpClient = new UdpClient(12345))
+            {
+                Console.WriteLine("Sever awaits for a message from the Client...");
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    ProcessMessage(udpClient);
+                });
+
+                Console.WriteLine("To close server press any key");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
+
+
