34eaaf68bf0004733bf4572357f861815e9a3a91
ksana-sidoruk <ksana.sidoruk@gmail.com>
2024-05-12 13:32:23 +0200

    HW4

---
+++ C#_Introduction/C#_NetworkingApplications/HW4_UdpServerWithCT/HW1_UdpClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using HW4_UdpServer;
+
+namespace HW4_UdpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Task.Run(() => SendMessage("This Computer", "127.0.0.1"));
+            Task.WaitAll(t);    
+        }
+
+        static void SendMessage(string from, string ipAddress)
+        {
+
+            using (var udpClient = new UdpClient())
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), 12345);
+
+                while (true)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("Please insert the message text or print \"EXIT\" to leave chat");
+                    string messageText = Console.ReadLine();
+
+                    Message message = new Message() { Text = messageText, NameFrom = from, NameTo = "Server", DateTime = DateTime.Now };
+
+                    string json = message.SerializeMessageToJson();
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+
+                    udpClient.Send(data, data.Length, ep);
+
+                    if (messageText.ToUpper() == "EXIT")
+                    {
+
+                        Console.WriteLine("The chat closes");
+                        Thread.Sleep(1000);
+                        udpClient.Close();
+                        break;
+                    }
+
+                    var serverEp = new IPEndPoint(IPAddress.Any, 0);
+                    var replyBuffer = udpClient.Receive(ref serverEp);
+                    var answer = Encoding.UTF8.GetString(replyBuffer);
+
+                    Console.WriteLine(answer);
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+    }
+}
---
+++ C#_Introduction/C#_NetworkingApplications/HW4_UdpServerWithCT/HW1_UdpServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HW4_UdpServer
+{
+    internal class Program
+    {
+        private static CancellationTokenSource _cts = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            CancellationToken ct = _cts.Token;
+
+            var t = Task.Run(()=>UdpServer(ct));
+            Task.WaitAll(t);
+        }
+
+        public static void ProcessMessage(UdpClient udpClient, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+                var buffer = udpClient.Receive(ref ep);
+
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                Task.Run(() =>
+                {
+                    Message message = Message.DeserializeMessageFromJson(messageText);
+                    message.Print();
+
+                    byte[] reply = Encoding.UTF8.GetBytes("\t\t\tMessage received at " + DateTime.Now);
+                    udpClient.Send(reply, ep);
+                });
+            }
+        }
+        public static void UdpServer(CancellationToken cancellationToken)
+        {
+            using (var udpClient = new UdpClient(12345))
+            {
+                Console.WriteLine("Sever awaits for a message from the Client...");
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    ProcessMessage(udpClient, cancellationToken);
+                });
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("To close server enter 'CLOSE' command");
+                    var command = Console.ReadLine();
+                    if(command != null && command.Equals("CLOSE", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        _cts.Cancel();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not supported command");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
